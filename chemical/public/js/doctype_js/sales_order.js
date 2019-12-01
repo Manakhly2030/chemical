38ef3f4a6f0d492207c163582128b047a86de080
frappe <frappe@ubuntu.vm>
2019-12-01 06:18:34 +0000

    Major Modifications

--- chemical/public/js/doctype_js/sales_order.js
+++ chemical/public/js/doctype_js/sales_order.js
@@ -22,3 +22,29 @@ frappe.ui.form.on("Sales Order", {
     },
     
 });
+
+cur_frm.fields_dict.items.grid.get_field("outward_sample").get_query = function(doc,cdt,cdn) {
+    let d = locals[cdt][cdn];
+    if(!d.item_code){
+        frappe.throw(__("Please select Item Code first."))
+    }
+	return {
+		filters: {
+            'docstatus':1,
+            "link_to":'Customer',
+            "product_name": d.item_code,
+            "party":doc.customer,
+            
+		}
+	}
+};
+
+frappe.ui.form.on("Sales Order Item", {
+    item_code: function(frm,cdt,cdn) {
+        console.log("call");
+        let d = locals[cdt][cdn];
+        frappe.model.set_value(d.doctype, d.name, 'outward_sample', "");
+		// frm.set_value('outward_sample',"")
+		// refresh_field('items');
+	}
+});
