30078a7f0eb0e2a48e6f105e73845bc9157bf252
frappe <frappe@ubuntu.vm>
2019-12-13 09:25:29 +0000

    Modified Inward Outward Count

--- chemical/api.py
+++ chemical/api.py
@@ -1163,6 +1163,78 @@ def calculate_total_planned_qty(self):
 def get_actual_and_projected_qty(warehouse,item_code):
 	qty = frappe.db.get_value("Bin", {"item_code": item_code, "warehouse": warehouse},["projected_qty", "actual_qty"], as_dict=True, cache=True)
 	return qty['actual_qty'] ,qty['projected_qty']
+
+
+@frappe.whitelist()
+def get_open_count(doctype, name, links):
+	'''Get open count for given transactions and filters
+
+	:param doctype: Reference DocType
+	:param name: Reference Name
+	:param transactions: List of transactions (json/dict)
+	:param filters: optional filters (json/list)'''
+
+	frappe.has_permission(doc=frappe.get_doc(doctype, name), throw=True)
+
+	meta = frappe.get_meta(doctype)
+	#links = meta.get_dashboard_data()
+
+	links = frappe._dict({
+		'fieldname': 'party',
+		'transactions': [
+			{
+				'label': _('Outward Sample'),
+				'items': ['Outward Sample']
+			},
+			{
+				'label': _('Inward Sample'),
+				'items': ['Inward Sample']
+			},
+		]
+	})
+    #frappe.msgprint(str(links))
+    #links = frappe._dict(links)
+    #return {'count':0}
+
+
+	# compile all items in a list
+	items = []
+	for group in links.transactions:
+		items.extend(group.get('items'))
+
+	out = []
+	for d in items:
+		if d in links.get('internal_links', {}):
+			# internal link
+			continue
+
+		filters = get_filters_for(d)
+		fieldname = links.get('non_standard_fieldnames', {}).get(d, links.fieldname)
+        #return fieldname
+		data = {'name': d}
+		if filters:
+			# get the fieldname for the current document
+			# we only need open documents related to the current document
+			filters[fieldname] = name
+			total = len(frappe.get_all(d, fields='name',
+				filters=filters, limit=100, distinct=True, ignore_ifnull=True))
+			data['open_count'] = total
+
+		total = len(frappe.get_all(d, fields='name',
+			filters={fieldname: name}, limit=100, distinct=True, ignore_ifnull=True))
+		data['count'] = total
+		out.append(data)
+
+	out = {
+		'count': out,
+	}
+
+	module = frappe.get_meta_module(doctype)
+	if hasattr(module, 'get_timeline_data'):
+		out['timeline_data'] = module.get_timeline_data(doctype, name)
+    
+	return out
+
 	
 # 	if isinstance(doc, string_types):
 # 		doc = json.loads(doc)
--- chemical/public/js/doctype_js/supplier.js
+++ chemical/public/js/doctype_js/supplier.js
@@ -1,4 +1,8 @@
 frappe.ui.form.on("Supplier", {
+	refresh: function(frm) {
+        dashboard_link_doctype(frm, "Outward Sample");
+        dashboard_link_doctype(frm, "Inward Sample");
+	},
 	onload_post_render: function(frm){
 		if(frm.doc.alias && !frm.doc.__islocal && frm.doc.name != frm.doc.alias){
 			frappe.call({
@@ -17,3 +21,90 @@ frappe.ui.form.on("Supplier", {
 		}
 	}
 });
+
+function dashboard_link_doctype(frm, doctype){
+
+	var parent = $('.form-dashboard-wrapper [data-doctype="Pricing Rule"]').closest('div').parent();
+	
+	parent.find('[data-doctype="'+doctype+'"]').remove();
+	
+	parent.append(frappe.render_template("dashboard_link_doctype", {doctype:doctype}));
+	
+	var self = parent.find('[data-doctype="'+doctype+'"]');
+	
+	set_open_count(frm, doctype);
+	
+	// bind links
+	self.find(".badge-link").on('click', function() {
+		frappe.route_options = {"quotation": frm.doc.name}
+		frappe.set_route("List", doctype);
+	});
+	
+	// bind open notifications
+	self.find('.open-notification').on('click', function() {
+		frappe.route_options = {
+			"quotation": frm.doc.name,
+			"status": "Draft"
+		}
+		frappe.set_route("List", doctype);
+	});
+	
+	// bind new
+	if(frappe.model.can_create(doctype)) {
+		self.find('.btn-new').removeClass('hidden');
+	}
+	self.find('.btn-new').on('click', function() {
+		frappe.new_doc(doctype,{
+			"quotation": frm.doc.name
+		});
+	});
+	}
+function set_open_count(frm, doctype){
+
+var method = '';
+var links = {};
+
+
+method = 'chemical.api.get_open_count';
+links = {
+	'fieldname': 'party',
+	'transactions': [
+		{
+			'label': __('Outward Sample'),
+			'items': ['Outward Sample']
+		},
+		{
+			'label': __('Inward Sample'),
+			'items': ['Inward Sample']
+		},
+	]
+};
+
+if(method!=""){
+	frappe.call({
+		type: "GET",
+		method: method,
+		args: {
+			doctype: frm.doctype,
+			name: frm.doc.name,
+			links: links,
+		},
+		callback: function(r) {
+			// update badges
+			$.each(r.message.count, function(i, d) {
+				frm.dashboard.set_badge_count(d.name, cint(d.open_count), cint(d.count));
+			});
+		}
+	});
+}
+}
+
+frappe.templates["dashboard_link_doctype"] = ' \
+<div class="document-link" data-doctype="{{ doctype }}"> \
+<a class="badge-link small">{{ __(doctype) }}</a> \
+<span class="text-muted small count"></span> \
+<span class="open-notification hidden" title="{{ __("Open {0}", [__(doctype)])}}"></span> \
+	<button class="btn btn-new btn-default btn-xs hidden" data-doctype="{{ doctype }}"> \
+			<i class="octicon octicon-plus" style="font-size: 12px;"></i> \
+	</button>\
+</div>';
