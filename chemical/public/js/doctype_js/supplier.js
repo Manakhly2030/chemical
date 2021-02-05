baba7e5a016e620ec0c0e66284084fdd1fc11356
Ubuntu <ubuntu@ip-172-31-16-250.ap-south-1.compute.internal>
2021-02-05 08:37:01 +0000

    changes in supplier and customer js

--- chemical/public/js/doctype_js/supplier.js
+++ chemical/public/js/doctype_js/supplier.js
@@ -1,7 +1,10 @@
 frappe.ui.form.on("Supplier", {
 	refresh: function(frm) {
-        dashboard_link_doctype(frm, "Outward Sample");
-        dashboard_link_doctype(frm, "Inward Sample");
+		if(!frm.doc.__islocal){
+
+			dashboard_link_doctype(frm, "Outward Sample");
+			dashboard_link_doctype(frm, "Inward Sample");
+		}
 	},
 	onload_post_render: function(frm){
 		if(frm.doc.alias && !frm.doc.__islocal && frm.doc.name != frm.doc.alias){
