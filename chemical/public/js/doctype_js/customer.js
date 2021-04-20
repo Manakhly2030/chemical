a6805cfee3907a98bc51c0da452a7d1146659c11
finbyz <info@finbyz.com>
2021-04-20 10:52:04 +0530

    CUstomer js changes

--- chemical/public/js/doctype_js/customer.js
+++ chemical/public/js/doctype_js/customer.js
@@ -1,9 +1,9 @@
 frappe.ui.form.on("Customer", {
 	refresh: function(frm) {
-		if(!frm.doc.__islocal){
-			dashboard_link_doctype(frm, "Outward Sample");
-			dashboard_link_doctype(frm, "Inward Sample");
-		}
+		// if(!frm.doc.__islocal){
+		// 	dashboard_link_doctype(frm, "Outward Sample");
+		// 	dashboard_link_doctype(frm, "Inward Sample");
+		// }
 	},
 	onload_post_render: function(frm){
 		if(frm.doc.alias && !frm.doc.__islocal && frm.doc.name != frm.doc.alias){
@@ -22,27 +22,26 @@ frappe.ui.form.on("Customer", {
 			})
 		}
 	},
-	before_save: function(frm){
-		console.log('call')
-		if((frm.doc.accounts == undefined || frm.doc.accounts.length == 0) && frm.doc.default_currency != undefined){
-            if (frm.doc.default_currency == 'USD') {
+	// before_save: function(frm){
+	// 	if((frm.doc.accounts == undefined || frm.doc.accounts.length == 0) && frm.doc.default_currency != undefined){
+    //         if (frm.doc.default_currency == 'USD') {
 				
-                let accounts = frm.add_child('accounts');
-                accounts.company = frappe.defaults.get_user_default("Company");
-                frappe.db.get_value("Company", accounts.company, 'abbr', function (r) {
-                    accounts.account = 'Debtor USD - ' + r.abbr;
-                });
-                }
-            else if(frm.doc.default_currency == 'EUR'){
-                let accounts = frm.add_child('accounts');
-                accounts.company = frappe.defaults.get_user_default("Company");
-                frappe.db.get_value("Company", accounts.company, 'abbr', function (r) {
-                    accounts.account = 'Debtors EUR - ' + r.abbr;
-                });
-            }
-			frm.refresh_field('accounts');
-		} 
-	}
+    //             let accounts = frm.add_child('accounts');
+    //             accounts.company = frappe.defaults.get_user_default("Company");
+    //             frappe.db.get_value("Company", accounts.company, 'abbr', function (r) {
+    //                 accounts.account = 'Debtor USD - ' + r.abbr;
+    //             });
+    //             }
+    //         else if(frm.doc.default_currency == 'EUR'){
+    //             let accounts = frm.add_child('accounts');
+    //             accounts.company = frappe.defaults.get_user_default("Company");
+    //             frappe.db.get_value("Company", accounts.company, 'abbr', function (r) {
+    //                 accounts.account = 'Debtors EUR - ' + r.abbr;
+    //             });
+    //         }
+	// 		frm.refresh_field('accounts');
+	// 	} 
+	// }
 
 });
 
