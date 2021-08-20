ba788734bb8b9cc9e246406bb99319ec6d9813be
Ubuntu <ubuntu@ip-172-31-10-161.ap-south-1.compute.internal>
2021-08-20 11:11:25 +0000

    Quality inspection js change

--- chemical/public/js/doctype_js/quality_inspection.js
+++ chemical/public/js/doctype_js/quality_inspection.js
@@ -1,21 +1,26 @@
-cur_frm.fields_dict['item_code'].get_query = function(doc, cdt, cdn) {
-	// const doctype = (doc.reference_type == "Stock Entry") ?
-	// 	"Stock Entry Detail" : doc.reference_type + " Item";
-    if(doc.reference_type == "Stock Entry"){
-       var doctype = "Stock Entry Detail"
-    }
-    else if(doc.reference_type == "Outward Sample"){
-        var doctype = "Outward Sample Detail"
-    }
-    else{
-        var doctype = doc.reference_type + " Item";
-    }
-	if (doc.reference_type && doc.reference_name && doc.reference_type!="Inward Sample") {
-		return {
-			query: "chemical.query.item_query",
-			filters: {
-				"from": doctype
+
+frappe.ui.form.on("Quality Inspection", {
+	setup: function(frm) {
+		frm.set_query("item_code", function(doc) {
+			// const doctype = (doc.reference_type == "Stock Entry") ?
+			// 	"Stock Entry Detail" : doc.reference_type + " Item";
+			if(doc.reference_type == "Stock Entry"){
+			var doctype = "Stock Entry Detail"
 			}
-		};
+			else if(doc.reference_type == "Outward Sample"){
+				var doctype = "Outward Sample Detail"
+			}
+			else{
+				var doctype = doc.reference_type + " Item";
+			}
+			if (doc.reference_type && doc.reference_name && doc.reference_type!="Inward Sample") {
+				return {
+					query: "chemical.query.item_query",
+					filters: {
+						"from": doctype
+					}
+				};
+			}
+		})
 	}
-}
+})
