c30262b08ea97f45f1ae82f5220d13674fddbc49
Ubuntu <ubuntu@ip-172-31-24-37.ap-south-1.compute.internal>
2022-03-28 10:33:37 +0000

    Window.open Link Changes

--- chemical/chemical/report/batch_wise_balance_chemical/batch_wise_balance_chemical.js
+++ chemical/chemical/report/batch_wise_balance_chemical/batch_wise_balance_chemical.js
@@ -52,5 +52,7 @@ frappe.query_reports["Batch Wise Balance Chemical"] = {
 	]
 }
 function view_stock_leder_report(item_code, from_date, to_date, batch_no) {
-	window.open(window.location.href.split('app')[0] + "app/query-report/Stock Ledger Chemical" + "/?" + "item_code=" + item_code  + "&" + "company= " + "&" + "from_date=" + from_date + "&" + "to_date=" + to_date + "&" + "batch_no=" + batch_no,"_blank")	
+	// window.open(window.location.href.split('app')[0] + "app/query-report/Stock Ledger Chemical" + "/?" + "item_code=" + item_code  + "&" + "company= " + "&" + "from_date=" + from_date + "&" + "to_date=" + to_date + "&" + "batch_no=" + batch_no,"_blank")
+	window.open(`/app/query-report/Stock Ledger Chemical/%3Fitem_code%3D${item_code}%26company%3D %26from_date%3D${from_date}%26to_date%3D${to_date}%26batch_no%3D${batch_no}`,"_blank")
+	
 }
