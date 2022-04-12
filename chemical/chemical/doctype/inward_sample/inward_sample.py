c6a5416af4ba503ab5e766c4c5c0f297c23a3deb
finbyz <info@finbyz.com>
2022-04-12 06:17:04 +0000

    First Commit

---
+++ chemical/chemical/doctype/inward_sample/inward_sample.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, FinByz Tech Pvt Ltd and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.model.document import Document
+from chemical.api import naming_series_name
+
+class InwardSample(Document):
+	def onclick_update_price(self):
+		# if self.link_to == "Supplier":
+		# if frappe.db.exists("Purchase Price" ,{ "product_name":self.item_code , "price_list":self.price_list}):
+			# purchase_price = frappe.get_doc("Purchase Price",{"product_name":self.item_code,"price_list":self.price_list})
+			# purchase_price.price = self.item_price
+			# purchase_price.price_list = self.price_list
+			# purchase_price.supplier = self.party
+			# purchase_price.save()
+			#purchase_price.run_method('on_update_after_submit')
+
+		# else:
+		if self.item_price != 0.00:
+			purchase_price = frappe.new_doc("Purchase Price")
+			purchase_price.ref_no = self.outward_reference
+			purchase_price.product_name = self.item_code
+			purchase_price.supplier_ref_no = self.ref_no
+			purchase_price.supplier_product_name = self.item_code
+			purchase_price.date = self.price_date or self.date
+			purchase_price.price = self.item_price
+			purchase_price.price_list = self.price_list
+			purchase_price.link_to = self.link_to
+			purchase_price.party = self.party
+			purchase_price.party_name = self.party_name
+
+			purchase_price.save()
+			# self.db_set('purchase_price', purchase_price.name)
+			purchase_price.submit()
+		frappe.db.commit()
+		frappe.msgprint(_("Purchase Price Updated"))
+
+	def before_save(self):
+		if self.link_to == "Customer" and self.party and self.item_code:
+			ref_code = frappe.db.get_value("Item Customer Detail", {'parent': self.item_code, 'customer_name': self.party}, 'ref_code')
+			self.customer_item_name = ref_code
+
+		
+	def before_naming(self):
+		if not self.amended_from:
+			if self.series_value:
+				if self.series_value > 0:
+					name = naming_series_name(self.naming_series)
+					
+					check = frappe.db.get_value('Series', name, 'current', order_by="name")
+					if check == 0:
+						pass
+					elif not check:
+						frappe.db.sql("insert into tabSeries (name, current) values ('{}', 0)".format(name))
+					
+					frappe.db.sql("update `tabSeries` set current = {} where name = '{}'".format(int(self.series_value) - 1,name))
+
