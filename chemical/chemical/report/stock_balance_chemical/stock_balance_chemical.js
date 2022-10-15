feb19d9a488c31b98965462292f9519e0f99edbb
Ubuntu <ubuntu@ip-172-31-22-45.ap-south-1.compute.internal>
2022-10-15 11:47:43 +0530

    Stock Ledger Chemical reference removed

--- chemical/chemical/report/stock_balance_chemical/stock_balance_chemical.js
+++ chemical/chemical/report/stock_balance_chemical/stock_balance_chemical.js
@@ -114,8 +114,8 @@ function view_batch_wise_report(item_code, company, to_date, warehouse) {
 	window.open(`/app/query-report/Batch Wise Balance Chemical/%3Fitem_code%3D${item_code}%26company%3D${company}%26to_date%3D${to_date}%26warehouse%3D${warehouse}`,"_blank")
 }
 function view_stock_leder_report(item_code,company, from_date, to_date, warehouse) {
-	// window.open(window.location.href.split('app')[0]+ "app/query-report/Stock Ledger Chemical" + "/?" + "item_code=" + item_code + "&" + "company=" + company + "&" + "from_date=" + from_date + "&" + "to_date=" + to_date + "&" + "warehouse=" + warehouse,"_blank")
-	window.open(`/app/query-report/Stock Ledger Chemical/%3Fitem_code%3D${item_code}%26company%3D${company}%26from_date%3D${from_date}%26to_date%3D${to_date}%26warehouse%3D${warehouse}`,"_blank")
+	// window.open(window.location.href.split('app')[0]+ "app/query-report/Stock Ledger" + "/?" + "item_code=" + item_code + "&" + "company=" + company + "&" + "from_date=" + from_date + "&" + "to_date=" + to_date + "&" + "warehouse=" + warehouse,"_blank")
+	window.open(`/app/query-report/Stock Ledger/%3Fitem_code%3D${item_code}%26company%3D${company}%26from_date%3D${from_date}%26to_date%3D${to_date}%26warehouse%3D${warehouse}`,"_blank")
 }
 $(window).on("load resize scroll",function(){
     setTimeout(function(){
