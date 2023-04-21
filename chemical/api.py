fb673f3a06b7fe8b573dd18f0765649333ed731c
Finbyz <info@finbyz.tech>
2023-04-21 17:43:51 +0530

    ch

--- chemical/api.py
+++ chemical/api.py
@@ -933,3 +933,28 @@ def make_batches(self, warehouse_field):
                     .insert()
                     .name
                 )
+
+def make_batches(self, warehouse_field):
+    """Create batches if required. Called before submit"""
+    for d in self.items:
+        if d.get(warehouse_field) and not d.batch_no:
+            has_batch_no, create_new_batch = frappe.db.get_value(
+                "Item", d.item_code, ["has_batch_no", "create_new_batch"]
+            )
+            if has_batch_no and create_new_batch:
+                d.batch_no = (
+                    frappe.get_doc(
+                        dict(
+                            doctype="Batch",
+                            item=d.item_code,
+                            supplier=getattr(self, "supplier", None),
+                            reference_doctype=self.doctype,
+                            reference_name=self.name,
+                            concentration=d.get('concentration'),
+                            valuation_rate=d.get('valuation_rate'),
+                            lot_no=d.get('lot_no')
+                        )
+                    )
+                    .insert()
+                    .name
+                )
