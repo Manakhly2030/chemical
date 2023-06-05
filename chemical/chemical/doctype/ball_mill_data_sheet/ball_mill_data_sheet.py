5f577107f4be6d34466d63a7a07ca151667756dc
Finbyz <info@finbyz.tech>
2023-06-05 14:28:20 +0530

    changes to resolved rate in ball mill data shit viral

--- chemical/chemical/doctype/ball_mill_data_sheet/ball_mill_data_sheet.py
+++ chemical/chemical/doctype/ball_mill_data_sheet/ball_mill_data_sheet.py
@@ -48,11 +48,11 @@ class BallMillDataSheet(Document):
 			if d.source_warehouse and d.batch_no:
 				args = self.get_args_for_incoming_rate(d)
 				d.basic_rate = get_incoming_rate((args), precision)
-			elif d.source_warehouse and not d.basic_rate:
+			if d.source_warehouse and not d.basic_rate:
 				d.basic_rate = get_valuation_rate(d.item_name, d.source_warehouse,
 					self.doctype, d.name, 1,
 					currency=erpnext.get_company_currency(self.company))
-			elif not d.source_warehouse:
+			if not d.source_warehouse:
 				d.basic_rate = 0.0
 			
 			d.basic_amount = d.basic_rate * d.qty
