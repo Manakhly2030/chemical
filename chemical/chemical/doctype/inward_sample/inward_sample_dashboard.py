34ab3651f2901850c082d4394cece4ab4ac32f0a
Finbyz <info@finbyz.tech>
2023-06-24 18:15:03 +0530

    changes

--- chemical/chemical/doctype/inward_sample/inward_sample_dashboard.py
+++ chemical/chemical/doctype/inward_sample/inward_sample_dashboard.py
@@ -3,18 +3,18 @@ from frappe import _
 def get_data():
 	return {
 		'fieldname': 'reference_name',
-		'non_standard_fieldnames': {
-			'Quality Inspection': 'reference_name',
-			'Product Research': 'inward_sample'
-		},
+		# 'non_standard_fieldnames': {
+		# 	'Quality Inspection': 'reference_name',
+		# 	'Product Research': 'inward_sample'
+		# },
 		'transactions': [
 			{	
 				'label': _('Quality Inspection'),
 				'items': ['Quality Inspection']
 			},	
-			{	
-				'label': _('Product Research'),
-				'items': ['Product Research']
-			},	
+			# {	
+			# 	'label': _('Product Research'),
+			# 	'items': ['Product Research']
+			# },	
 		]
 	}
