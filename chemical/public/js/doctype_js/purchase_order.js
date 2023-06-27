10e9ae13c57fe9941ebc3337b685cd164bcb634c
Ubuntu <ubuntu@ip-172-31-28-176.ap-south-1.compute.internal>
2023-06-27 06:57:18 +0000

    some older push from ushanti and patch updated by SS

--- chemical/public/js/doctype_js/purchase_order.js
+++ chemical/public/js/doctype_js/purchase_order.js
@@ -1,16 +1,3 @@
-
-// Add searchfield to Item query
-this.frm.cscript.onload = function(frm) {
-	this.frm.set_query("item_code", "items", function() {
-		return {
-			query: "chemical.query.new_item_query",
-			filters: {
-				'is_sales_item': 1
-			}
-		}
-	});
-}
-
 cur_frm.fields_dict.taxes_and_charges.get_query = function(doc) {
 	return {
 		filters: {
